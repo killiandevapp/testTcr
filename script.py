6377eb5b4fb8149d05635be83fd4e50f74b6ebf2
m-ornella <marieornella.vitry@gmail.com>
2024-03-13 16:30:01 +0100

    total price to zero

--- script.py
+++ script.py
@@ -1,2 +1,5 @@
 def hello_world():
     return "Hello, World!"
+
+def print_total_price_zero():
+    return "Total price : 0€"
--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import unittest
-from script import hello_world
+from script import hello_world, print_total_price_zero
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, World!")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class TestTotalPriceZero(unittest.TestCase):
+    def test_print_total_price_zero(self):
+        self.assertEqual(print_total_price_zero(), "Total price : 0€")
 
 if __name__ == '__main__':
     unittest.main()
