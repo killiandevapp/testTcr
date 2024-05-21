401bd9f0508073eb29b6eea1399380344498c3b9
Trang <nguyenthuytrang.fr@gmail.com>
2024-05-21 16:04:08 +0200

    ...

--- src/script.py
+++ src/script.py
@@ -1,19 +1,22 @@
 def hello_world():
     return "Hello, World!"
 
+assert hello_world() == "Hello, World!"
+"""
 def print_total_price_zero():
     return "Total price : 0€"
 
 def add_product_print():
     return "Do you want to add the product ? Yes/No"
-    """
+    
     answer = input()
     if input == "Yes" or "yes": 
         return 
-        """
+        
 def calculer_prix_total(prix_unitaire, quantite):
     return prix_unitaire * quantite
 
 prix_unitaire = float(input("Entrez le prix unitaire de l'article : "))
 quantite = int(input("Entrez la quantité de l'article : "))
 prix_total = calculer_prix_total(prix_unitaire, quantite)
+"""
